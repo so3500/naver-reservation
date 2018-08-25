a6ed53face7fcefb0c25f2311cf260f97e028f3b
so3500 <dltjddhs@gmail.com>
2018-08-25 17:19:41 +0900

    refactor: ajaxUtil의 status code 에 따른 대응 세분화
    GET, POST, PUT 의 성공 상태코드에 따라서 대응 세분화

--- src/main/webapp/js/util/ajax.util.js
+++ src/main/webapp/js/util/ajax.util.js
@@ -4,7 +4,13 @@ AjaxUtil.prototype.sendGetAjax = function(url) {
 	return new Promise((resolve, reject) => {
 		const req = new XMLHttpRequest();
 		req.open("GET", url);
-		req.onload = () => resolve(req.responseText);
+	    req.onload = function () {
+	        if (req.status === 200) {
+	          resolve(req.responseText);
+	        } else {
+	          reject(req.status);
+	        }
+	    };
 		req.onerror = () => reject(req.status);
 		req.send();
 	});
@@ -15,7 +21,13 @@ AjaxUtil.prototype.sendPostAjax = function(url, data) {
 		const req = new XMLHttpRequest();
 		req.open("POST", url);
 		req.setRequestHeader("Content-type", "application/json;charset=utf-8");
-		req.onload = () => resolve(req.responseText);
+	    req.onload = function () {
+	        if (req.status === 200 || req.status === 201) {
+	          resolve(req.responseText);
+	        } else {
+	          reject(req.status);
+	        }
+	    };
 		req.onerror = () => reject(req.status);
 		req.send(data);
 	});
@@ -25,7 +37,13 @@ AjaxUtil.prototype.sendPutAjax = function(url) {
 	return new Promise((resolve, reject) => {
 		const req = new XMLHttpRequest();
 		req.open("PUT", url);
-		req.onload = () => resolve(req.responseText);
+	    req.onload = function () {
+	        if (req.status === 200 || req.status === 202 || req.status === 204) {
+	          resolve(req.responseText);
+	        } else {
+	          reject(req.status);
+	        }
+	    };
 		req.onerror = () => reject(req.status);
 		req.send();
 	});
